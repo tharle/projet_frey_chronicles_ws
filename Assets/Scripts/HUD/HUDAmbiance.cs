4efd69f5e740355a0fb20aafb01f681573a61153
Tharle JC <tharles.josefidecamargo@gmail.com>
2024-06-25 23:34:16 -0400

    Add some new lighting and some polish, battle mode, change for music ambiance to battle, victory music

---
+++ Assets/Scripts/HUD/HUDAmbiance.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDAmbiance : MonoBehaviour
+{
+    [SerializeField] GameObject m_BattleModeDisplay;
+    [SerializeField] EAudio m_AmbianceMusic;
+    private AudioSource m_MusicPlaying;
+    private float m_Volume = 0.5f;
+
+    private void Start()
+    {
+        GameEventSystem.Instance.SubscribeTo(EGameEvent.BattleMode, OnBattleMode);
+        m_MusicPlaying = AudioManager.Instance.Play(EAudio.MusicAmbient_1, Camera.current.transform.position, true);
+        m_MusicPlaying.volume = m_Volume;
+    }
+
+    private void OnBattleMode(GameEventMessage message)
+    {
+        if(message.Contains<bool>(EGameEventMessage.Enter, out bool enter))
+        {
+            m_BattleModeDisplay.SetActive(enter);
+            
+            m_MusicPlaying.Stop();
+            if (enter) m_MusicPlaying = AudioManager.Instance.Play(EAudio.MusicFight, Camera.current.transform.position, true);
+            else  m_MusicPlaying = AudioManager.Instance.Play(EAudio.MusicAmbient_1, Camera.current.transform.position, true);
+
+            m_MusicPlaying.volume = m_Volume;
+        }
+    }
+}
