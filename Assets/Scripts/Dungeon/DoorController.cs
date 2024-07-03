95d4554fd44a43b2034f2a979556c553e876f628
Tharle Josefi de Camargo <tharles.josefidecamargo@gmail.com>
2024-07-03 14:47:07 -0400

    Add arrow indicator for door direction (be visible even if wall is invisible)

--- Assets/Scripts/Dungeon/DoorController.cs
+++ Assets/Scripts/Dungeon/DoorController.cs
@@ -1,9 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+
+    [SerializeField] private GameObject m_Arrow;
+    [SerializeField] private GameObject m_Block;
+
+    private void Start()
+    {
+        SubscribeAll();
+    }
+
+    private void SubscribeAll()
+    {
+        GameEventSystem.Instance.SubscribeTo(EGameEvent.BattleMode, OnBattleMode);
+    }
+
+    private void OnBattleMode(GameEventMessage message)
+    {
+        if(message.Contains<bool>(EGameEventMessage.Enter, out bool enter))
+        {
+            m_Arrow.SetActive(!enter);
+            m_Block.SetActive(enter);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag(GameParametres.TagName.PLAYER))
